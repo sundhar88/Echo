6bd5dba878e2a8306821bcf5d66e0ed859409b88
Aravind RaJ <aravinu19@gmail.com>
2018-09-22 06:03:36 +0530

    Update MainActivity.java

--- ECHO/app/src/main/java/com/ivara/aravi/echo/MainActivity.java
+++ ECHO/app/src/main/java/com/ivara/aravi/echo/MainActivity.java
@@ -26,9 +26,9 @@ import com.quickblox.users.model.QBUser;
 public class MainActivity extends AppCompatActivity {
 
     static final String APP_ID = "61145";
-    static final String AUTH_KEY = "783YOzRzR4gw-YJ";
-    static final String AUTH_SECRET = "WDC5VeK9k-g4T-A";
-    static final String ACCOUNT_KEY = "uWDNYGzogwZbA1fyZ9Kx";
+    static final String AUTH_KEY = "Your-QuickBlox-AUTH-KEY";
+    static final String AUTH_SECRET = "Your-QuickBlox-AUTH-SECRET";
+    static final String ACCOUNT_KEY = "Your-QuickBlox-ACCOUNT-KEY";
 
     static final int REQUEST_CODE = 1000;
 
